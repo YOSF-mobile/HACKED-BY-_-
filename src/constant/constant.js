361e77b89e1d06a588f1bb5b4b283f29fffe599c
YOSF-mobile <chnarzebari860@gmail.com>
2026-07-02 21:00:03 +0300

    Add files via upload

---
+++ src/constant/constant.js
@@ -0,0 +1,382 @@
+import gpt from '../../public/img/threejs.png';
+import ggg from '../../public/img/ggg.png';
+import arabicF from '../../public/img/arabicshit.png';
+import beenar from '../../public/img/bennar.png';
+import tim from '../../public/img/tim.png';
+import fel3arda from '../../public/img/fel3arda.png';
+import flix from '../../public/img/flix.png';
+import gg from '../../public/img/gg.png';
+import doblazh from '../../public/img/kd-logoo.png';
+import kurdcinama from '../../public/img/kurdcinama.png';
+import kurdsubtitle from '../../public/img/kurdsubtitlenew.png';
+import nextui from '../../public/img/nextui.png';
+import red from '../../public/img/red.png';
+
+import shacnui from '../../public/img/shacnui.png';
+import today from '../../public/img/today.png';
+import topfotbalarabic from '../../public/img/topfotbalarabic.png';
+
+import watchblue from '../../public/img/watchblue.png';
+import yallaSport from '../../public/img/yalla sport.png';
+
+export const MovieSites = {
+  Sites: [
+    {
+      id: "1",
+      name: "کورد سینەما",
+      image: kurdcinama,
+      url: "https://www.kurdcinama.com/",
+      tooltip: "visit the site",
+      category: "Kurdish"
+    },
+    {
+      id: "2",
+      name: "كورد سەبتایتڵ",
+      image: kurdsubtitle,
+      url: "https://kurdsubtitle.net/",
+      tooltip: "visit the site",
+      category: "Kurdish"
+    },
+    {
+      id: "3",
+      name: "بینەر",
+      image: beenar,
+      url: "https://beenar.net/",
+      tooltip: "visit the site",
+      category: "Kurdish"
+    },
+    {
+      id: "4",
+      name: "Badini",
+      image: arabicF,
+      url: "https://badini.shafilm.vip/",
+      tooltip: "visit the site",
+      category: "Kurdish"
+    },
+    {
+      id: "5",
+      name: "Kurddoblazh",
+      image: doblazh,
+      url: "https://www.kurddoblazh.com/",
+      tooltip: "visit the site",
+      category: "Kurdish"
+    }
+  ],
+
+  ArabicSites: [
+    {
+      id: "1",
+      name: "Shahid",
+      image: arabicF,
+      url: "https://shahid.mbc.net/en",
+      tooltip: "visit the site",
+      category: "Arabic"
+    },
+    {
+      id: "2",
+      name: "Filmdoo",
+      image: red,
+      url: "https://www.filmdoo.com/arabic/page/1",
+      tooltip: "visit the site",
+      category: "Arabic"
+    }
+  ],
+
+  English: [
+    {
+      id: "1",
+      name: "WatchSeries",
+      image: watchblue,
+      url: "https://watchseries.bar/",
+      tooltip: "visit the site",
+      category: "English"
+    },
+    {
+      id: "2",
+      name: "WatchSeriesHD",
+      image: gg,
+      url: "https://www1.watchserieshd.org/",
+      tooltip: "visit the site",
+      category: "English"
+    },
+    {
+      id: "3",
+      name: "WatanFlix",
+      image: flix,
+      url: "https://watanflix.com/en",
+      tooltip: "visit the site",
+      category: "English"
+    },
+    {
+      id: "4",
+      name: "HDToday",
+      image: today,
+      url: "https://hdtodayz.to/",
+      tooltip: "visit the site",
+      category: "English"
+    }
+  ],
+
+  Sports: [
+    {
+      id: "1",
+      name: "Marca",
+      image: arabicF,
+      url: "https://www.marca.com/en/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "2",
+      name: "ESPN",
+      image: "",
+      url: "https://www.espn.com/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "3",
+      name: "Fal3arda",
+      image: fel3arda,
+      url: "https://fal3arda-new.com/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "4",
+      name: "Shootz",
+      image: yallaSport,
+      url: "https://shootz.yalla-shoot-tv.live/fel3ardas/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "5",
+      name: "Yari",
+      image: "",
+      url: "http://wvvw.yari.live/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "6",
+      name: "YariGaTV",
+      image: "",
+      url: "https://yarigatv.basnews.com/",
+      tooltip: "visit the site"
+    }
+  ],
+
+  Programming: [
+    // UI
+    {
+      id: "1",
+      name: "GLTF",
+      image: gpt,
+      url: "https://gltf.pmnd.rs/",
+      tooltip: "visit the site",
+      category: "UI"
+    },
+    {
+      id: "2",
+      name: "Creative Tim",
+      image: tim,
+      url: "https://www.creative-tim.com/twcomponents/gradient-generator",
+      tooltip: "visit the site",
+      category: "UI"
+    },
+    {
+      id: "3",
+      name: "Flowbite",
+      image: shacnui,
+      url: "https://flowbite.com/docs/components/drawer/",
+      tooltip: "visit the site",
+      category: "UI"
+    },
+    {
+      id: "13",
+      name: "Tailwind UI",
+      image: tim,
+      url: "https://tailwindui.com/",
+      tooltip: "visit the site",
+      category: "UI"
+    },
+    {
+      id: "5",
+      name: "Three.js",
+      image: gpt,
+      url: "https://threejs.org/",
+      tooltip: "visit the site",
+      category: "UI"
+    },
+    {
+      id: "6",
+      name: "GSAP",
+      image: ggg,
+      url: "https://gsap.com/",
+      tooltip: "visit the site",
+      category: "UI"
+    },
+    {
+      id: "7",
+      name: "MUI",
+      image: nextui,
+      url: "https://mui.com/",
+      tooltip: "visit the site",
+      category: "UI"
+    },
+    {
+      id: "8",
+      name: "Shadcn",
+      image: shacnui,
+      url: "https://ui.shadcn.com/",
+      tooltip: "visit the site",
+      category: "UI"
+    },
+    {
+      id: "9",
+      name: "NextUI",
+      image: nextui,
+      url: "https://nextui.org/docs/guide/introduction",
+      tooltip: "visit the site",
+      category: "UI"
+    },
+    
+    // Data Structure
+    {
+      id: "4",
+      name: "Frontend Masters",
+      image: "",
+      url: "https://frontendmasters.com/courses/algorithms/bubble-sort/",
+      tooltip: "visit the site",
+      category: "Data Structure"
+    },
+    {
+      id: "14",
+      name: "GeeksforGeeks",
+      image: arabicF,
+      url: "https://www.geeksforgeeks.org/data-structures/",
+      tooltip: "visit the site",
+      category: "Data Structure"
+    },
+    
+    // Tools
+    {
+      id: "10",
+      name: "Git",
+      image: arabicF,
+      url: "https://git-scm.com/",
+      tooltip: "visit the site",
+      category: "Tools"
+    },
+    {
+      id: "11",
+      name: "GitHub",
+      image: arabicF,
+      url: "https://github.com/",
+      tooltip: "visit the site",
+      category: "Tools"
+    },
+    {
+      id: "12",
+      name: "Next.js",
+      image: nextui,
+      url: "https://nextjs.org/",
+      tooltip: "visit the site",
+      category: "Tools"
+    },
+    {
+      id: "16",
+      name: "Vite",
+      image: nextui,
+      url: "https://vitejs.dev/",
+      tooltip: "visit the site",
+      category: "Tools"
+    }
+  ],
+  Education: [
+    {
+      id: "1",
+      name: "Salahaddin University",
+      image:  "",
+      url: "https://ids.su.edu.krd/account/login?returnUrl=%2Fconnect%2Fauthorize%2Fcallback%3Fclient_id%3Dsalahaddin-Uni.webapp1%26redirect_uri%3Dhttps%253A%252F%252Fapp.su.edu.krd%252Ferp-web-signin-oidc%26response_type%3Dcode%2520id_token%26scope%3Dopenid%2520profile%2520roles%2520hrapi%2520offline_access%26response_mode%3Dform_post%26nonce%3D638598612927837485.OTAzNjM0NWItYTc4MS00ZTcyLWEyOWQtODU3ZWI1YTE1NDBkMDA1ZmJmMzUtYTNiMC00Zjk5LTgxMDctNWViMDc1MzUzMTIz%26state%3DCfDJ8LxI8ChCUGZMlKEGZXqDsFQ4m4uFg-9d9_1yqhqoYf4voTmJmJjeNSfJXCGBbV5kPdbKkE83BDHjJ-R5iJqO-VXgv-kg6bRIXEKD0m3xRlmo9rCkWXBR33t09K__d-vbjzBk2OFCJtiBPSe1eorvY7EbaILZFzaR8ZPpe5ml4XVLLatuHnVNROnrz1BywvaBQoKK7pDwrvuS7ZTl15xQ9iit_elw-d-7vlwhPj0UnVmZJb5lmFozHxuzYuoBxN-7N6gjXBapss_hPfS3qsVguri9duIyEkeWk9wCmfjOSE3BdqTO_5jZBzdSPzg0HFcnmA%26x-client-SKU%3DID_NETSTANDARD1_4%26x-client-ver%3D5.2.0.0",
+      tooltip: "visit the site"
+    },
+    {
+      id: "2",
+      name: "Duolingo",
+      image: topfotbalarabic,
+      url: "https://www.duolingo.com/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "3",
+      name: "QuillBot",
+      image: "",
+      url: "https://quillbot.com/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "4",
+      name: "Gamma",
+      image: "",
+      url: "https://gamma.app/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "5",
+      name: "ChatGPT",
+      image: "",
+      url: "https://chatgpt.com/",
+      tooltip: "visit the site"
+    }
+  ],
+
+
+  Social: [
+    {
+      id: "1",
+      name: "Facebook",
+      image: red,
+      url: "https://www.facebook.com/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "2",
+      name: "TikTok",
+      image: "",
+      url: "https://www.tiktok.com/en/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "3",
+      name: "Telegram",
+      image: arabicF,
+      url: "https://web.telegram.org/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "4",
+      name: "Instagram",
+      image: red,
+      url: "https://www.instagram.com/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "5",
+      name: "Snapchat",
+      image: red,
+      url: "https://www.snapchat.com/",
+      tooltip: "visit the site"
+    },
+    {
+      id: "6",
+      name: "X",
+      image: arabicF,
+      url: "https://x.com/home",
+      tooltip: "visit the site"
+    },
+    {
+      id: "7",
+      name: "Pinterest",
+      image: arabicF,
+      url: "https://www.pinterest.com/",
+      tooltip: "visit the site"
+    }
+  ]
+};
